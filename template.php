bd6b8c4aa0ab3d9301282c9754e7f8fd9ad87d4b
Cliff Ingham <inghamn@bloomington.in.gov>
2013-05-20 18:05:00 +0000

    Initial import of COB theme - work in progress

---
+++ template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright 2013 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+function cob_preprocess_node(&$variables)
+{
+	$variables['submitted'] = t('!date', array('!date'=>$variables['date']));
+}
