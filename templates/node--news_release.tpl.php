8e9eadbd706bcde6451f72a49c657e76529aba13
Cliff Ingham <inghamn@bloomington.in.gov>
2013-05-27 15:30:46 +0000

    Removed clearfix from News Release node template

--- templates/node--news_release.tpl.php
+++ templates/node--news_release.tpl.php
@@ -80,7 +80,7 @@
  * @ingroup themeable
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
 
@@ -98,12 +98,12 @@
   <?php endif; ?>
 
   <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
+	<?php
+		// We hide the comments and links now so that we can render them later.
+		hide($content['comments']);
+		hide($content['links']);
+		print render($content);
+	?>
   </div>
 
   <?php print render($content['links']); ?>
