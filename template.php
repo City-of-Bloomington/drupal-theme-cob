96615c6c4008e2c862424d70f84a0c04f8cb03b9
Cliff Ingham <inghamn@bloomington.in.gov>
2013-05-27 15:29:45 +0000

    Customized breadcrumb template

--- template.php
+++ template.php
@@ -8,3 +8,17 @@ function cob_preprocess_node(&$variables)
 {
 	$variables['submitted'] = t('!date', array('!date'=>$variables['date']));
 }
+
+/**
+ * Override of theme_breadcrumb().
+ */
+function cob_breadcrumb($variables) {
+	$breadcrumb = $variables['breadcrumb'];
+
+	array_shift($breadcrumb); // Remove the Home link
+
+	if (!empty($breadcrumb)) {
+		$output = '<div class="breadcrumb">' . implode('', $breadcrumb) . '</div>';
+		return $output;
+	}
+}
