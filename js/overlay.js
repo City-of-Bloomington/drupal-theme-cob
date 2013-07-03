00786ff084522e949f4b67cca5a40061e180e36a
Cliff Ingham <inghamn@bloomington.in.gov>
2013-07-03 17:24:04 +0000

    Javascript is now setting the CSS classes for the megadropdowns

--- js/overlay.js
+++ js/overlay.js
@@ -1,49 +1,13 @@
-
-$(document).ready(function(){
-			$(".nav-button").click(function () {
-			$(".nav-button,.primary-nav").toggleClass("open");
-			});    
-		});
-
-$(function() {
-    var $mainNav = $('#menuBar'),
-	    navWidth = $mainNav.width();
-		
-	$(document).ready(function(){
-		var ua = navigator.userAgent;
-		function is_touch_device(){
-			try {
-				document.createEvent("TouchEvent");
-				return true;
-			} catch (e) {
-				return false;
-			}
-		}
-	
-		if (is_touch_device() == false) {
-				$mainNav.children('.main-nav-item').hover(function(ev) {
-					var $this = $(this),
-					$dd = $this.find('.megadropdown-region');
-			
-				   
-					// get the width of the dropdown
-					var ddWidth = $dd.width(),
-					leftMax = navWidth - ddWidth;
-			
-					// show the dropdown
-					$this.addClass('main-nav-item-active');
-				}, function(ev) {
-					// hide the dropdown
-					$(this).removeClass('main-nav-item-active');
-				});
-		}
+jQuery(document).ready(function($) {
+	$(".nav-button").click(function () {
+		$(".nav-button,.primary-nav").toggleClass("open");
 	});
 
+	$('#menubar .main-nav-item').on('mouseover', function() {
+		$(this).addClass('main-nav-item-active');
+	});
+
+	$('#menubar .main-nav-item').on('mouseleave', function() {
+		$(this).removeClass('main-nav-item-active');
+	});
 });
-
-
-
- // get the left position of this tab
-    //    var leftPos = $this.find('.main-nav-item').position().left;
-		// position the dropdown
-//        $dd.css('left', Math.min(leftPos, leftMax));
