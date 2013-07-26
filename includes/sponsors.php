ce76e4d5a2697c8a402668c9bdd53f034c481cbb
Cliff Ingham <inghamn@bloomington.in.gov>
2013-07-26 15:32:45 +0000

    Sponsors sidebar include now renders using Drupals dispay settings for that view mode.

--- includes/sponsors.php
+++ includes/sponsors.php
@@ -10,21 +10,8 @@ echo "
 	<h2>Sponsors</h2>
 ";
 	foreach ($sponsors as $s) {
-		echo "<div class=\"item\">";
-		if (isset($s['entity']->field_logo['und'][0]['uri'])) {
-			echo theme('image_style', array(
-				'path'=>$s['entity']->field_logo['und'][0]['uri'],
-				'style_name'=>'thumbnail'
-			));
-		}
-		echo "
-			".l($s['entity']->title, "node/$s[target_id]")."
-		";
-			$u = $s['entity']->field_link_url['und'][0]['url'];
-		echo "
-			".l($u, $u)."
-		</div>
-		";
+		$n = entity_view('node', [$s['entity']], 'sidebar');
+		echo render($n);
 	}
 echo "
 </div>
