1b940e5d86f5e0c3cd20367dffffa5c55e2a1c1a
Emily Brown <browne@bloomington.in.gov>
2013-11-26 15:50:03 +0000

    moved dropdowns to a separate php file

---
+++ includes/dropdowns.php
@@ -0,0 +1,272 @@
+		<ul class="megadropdown primary-nav">
+				<li class="main-nav-item">
+					<a href="/drupal/node/127" class="withDD main-nav-tab">My Government</a>
+					<div class="megadropdown-region" id="governmentItem">
+						<div class="smleft">
+								<div class="fourcolumns">
+									<ul>
+									<li><ul class="item">
+										<li><h4>Mayor Mark Kruzan</h4></li>
+										<li><a href="/news">City News</a></li>
+										<li>State of the City Address</li>
+										<li>Proclamations</li>
+										<li>More</li>
+									</ul></li>
+									<li><ul class="item">
+										<li><h4>Bloomington Common Council</h4></li>
+										<li>Meeting Schedule and Agenda</li>
+										<li>Meeting Minutes</li>
+										<li>Contacting the Council</li>
+										<li>Find your Council Representative</li>
+									</ul></li>
+									<li><ul class="item">
+										<li><h4>Topics</h4></li>
+										<li>Citizen Involvement</li>
+										<li>City Projects</li>
+										<li>City Initiatives</li>
+									</ul></li>
+									<li><ul class="item">
+										<li>Doing Business with the City</li>
+										<li>Employment Opportunities</li>
+										<li>Municipal Code</li>
+										<li>See more about Government</li>
+									</ul></li>
+									</ul>
+								</div>
+								<div class="smbottom">
+									<h4>City Boards and Commissions</h4>
+									<form action="URL">
+										<fieldset>
+										<select name="URL" onchange="window.location.href= this.form.URL.options[this.form.URL.selectedIndex].value">
+											<option>Animal Control Commission</option>
+											<option>Bicycle and Pedestrian Safety Commission</option>
+											<option>Bloomington Community Arts Commission</option>
+											<option>Bloomington Digital Underground Advisory Committee</option>
+										</select>
+										</fieldset>
+									</form>
+									<h4>City Departments</h4>
+									<form action="URL">
+										<fieldset>
+									<select name="URL" onchange="window.location.href= this.form.URL.options[this.form.URL.selectedIndex].value">
+									<option value="">Select a department</option>
+									<option value="/drupal/content/animal-care-and-control">Animal Care and Control</option>
+									<option value="">Bloomington Transit</option>
+									<option value="">City Clerk</option>
+									<option value="">Community and Family Resources (CFRD)</option>
+									<option value="">Controller</option>
+									<option value="">Council Office</option>
+									<option value="">Economic &amp; Sustainable Development</option>
+									<option value="">Engineering</option>
+									<option value="">Fire</option>
+									<option value="">Housing and Neighborhood Development (HAND)</option>
+									<option value="">Human Resources</option>
+									<option value="">Information &amp; Technology Services (ITS)</option>
+									<option value="">Legal</option>
+									<option value="">Office of the Mayor (OOTM)</option>
+									<option value="">Parking Enforcement</option>
+									<option value="">Parks and Recreation</option>
+									<option value="">Planning</option>
+									<option value="">Police</option>
+									<option value="">Public Works</option>
+									<option value="">Risk Management</option>
+									<option value="">Sanitation</option>
+									<option value="">Street</option>
+									<option value="">Traffic</option>
+									<option value="">Utilities</option>
+									</select>
+										</fieldset>
+								</form>
+								</div>
+						</div>
+						<div class="smright">
+							<p>
+								<img src="http://rogue.bloomington.in.gov/drupal/sites/default/files/contactrectangle.png" />
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="main-nav-item">
+					<a href="/drupal/node/129" class="main-nav-tab withDD">I Want to</a>
+
+					<div class="megadropdown-region">
+						<div class="smleft">
+							<div class="fourcolumns">
+								<ul>
+									<li><ul class="item">
+										<li><h4>Apply</h4></li>
+										<li>City Jobs</li>
+										<li>Board & Commissions</li>
+										<li>Foster a shelter animal</li>
+										<li>Pet behavior consultation</li>
+									</ul></li>
+									<li><ul class="item">
+										<li><h4>Pay</h4></li>
+										<li>Parking Ticket</li>
+										<li>Utility Bill</li>
+										<li>Parks & Recreation Dept Facilities</li>
+										<li>Park Shelterhouses</li>
+										<li>Tee Time</li>
+									</ul></li>
+									<li><ul class="item">
+										<li><h4>Sign Up</h4></li>
+										<li>Citizen's Academy</li>
+										<li>Parks & Recreation Dept programs</li>
+										<li>Utilities Services</li>
+									</ul></li>
+									<li><ul class="item">
+										<li><h4>Report</h4></li>
+										<li>Things related to:</li>
+										<li>Land and Housing</li>
+										<li>Traffic, Roads and Sidewalks</li>
+										<li>Parks & Trails</li>
+										<li>Utilities</li>
+										<li>Quality of Living, City Comments</li>
+									</ul></li>
+								</ul>
+							</div>
+						</d 
+					</div>
+				</li>
+				<li class="main-nav-item">
+					<a href="/drupal/node/128" class="main-nav-tab withDD">Learn About</a>
+
+					<div class="megadropdown-region">
+						<div class="smleft">
+							<div class="fourcolumns">
+								<ul>
+									<li><ul class="item">
+										<li>About Bloomington</li>
+										<li>Accessibility</li>
+										<li><?php echo l("Animals", "node/91"); ?></li>
+										<li>Arts & Culture</li>
+										<li>Biking and Walking</li>
+										<li>Business</li>
+										<li>Civic Engagement</li>
+										<li>Community Events</li>
+									</ul></li>
+									<li><ul class="item">
+										<li>Diversity</li>
+										<li>Employment</li>
+										<li><?php echo l("Farmers Market", "node/40"); ?></li>
+										<li>Grants and Funding</li>
+										<li>Health Resources</li>
+										<li>Historic Preservation</li>
+										<li><?php echo l("Housing", "node/93"); ?></li>
+									</ul></li>
+									<li><ul class="item">
+										<li>Legislation</li>
+										<li>Maps, GIS, Location Tools</li>
+										<li>Neighborhoods</li>
+										<li><a href="/news">News</a></li>
+										<li><?php echo l("Parking", "node/92"); ?></li>
+										<li><?php echo l("Parks & Recreation Programs", "node/103"); ?></li>
+									</ul></li>
+									<li><ul class="item">
+										<li>Public Safety</li>
+										<li>Residential Services</li>
+										<li>Roads and Traffic</li>
+										<li><?php echo l("Sports & Fitness", "node/102"); ?></li>
+										<li>Sustainable City</li>
+										<li>Trash and Recycling</li>
+										<li>Volunteering</li>
+									</ul></li>
+								</ul>
+							</div>
+						<div class="smbottom">
+							<h4>Community Facilities</h4>
+							<p>Directions, hours, other info</p>
+							<form action="URL">
+								<fieldset>
+									<select name="URL" onchange="window.location.href= this.form.URL.options[this.form.URL.selectedIndex].value">
+										<option value="">Allison-Jukebox Community Center</option>
+										<option value="">Banneker Community Center</option>
+										<option value="">Bryan Park Pool</option>
+										<option value="">Cascades Golf Course</option>
+										<option value="">Frank Southern Ice Arena</option>
+										<option value="">Griffy Lake Boathouse</option>
+										<option value="">Mills Pool</option>
+										<option value="">Skate Park</option>
+										<option value="">Twin Lakes Recreation Center</option>
+										<option value="">Twin Lakes Sports Park</option>
+										<option>Winslow Sports Complex</option>
+									</select>
+								</fieldset>
+							</form>
+							<h4>Parks, Trails and Gardens</h4>
+							<p>Directions, playground/shelter offerings, other info</p>
+							<form action="URL">
+								<fieldset>
+								<select name="URL" onchange="window.location.href= this.form.URL.options[this.form.URL.selectedIndex].value">
+								<option value="">Broadview Park</option>
+								<option value="/drupal/content/bryan-park">Bryan Park</option>
+								<option value="">Building Trades Park</option>
+								<option value="">Clear Creek Trail</option>
+								<option value="">Crestmont Park &amp; Garden</option>
+								<option value="">Goat Farm</option>
+								<option value="">Griffy Lake Nature Preserve</option>
+								<option value="">Highland Village Park</option>
+								<option value="">Latimer Woods</option>
+								<option value="">Leonard Springs Nature Park</option>
+								<option value="">Lower Cascades Park</option>
+								<option value="">Miller-Showers Park</option>
+								<option value="">Olcott Park</option>
+								<option value="">Park Ridge East Park</option>
+								<option value="">Park Ridge Park</option>
+								<option value="">Peoples Park</option>
+								<option value="">RCA Park</option>
+								<option value="">Rev Ernest D Butler Park</option>
+								<option value="">Schmalz Farm Park</option>
+								<option value="">Seminary Square Park</option>
+								<option value="">Sherwood OaksPark</option>
+								<option value="">Skate Park</option>
+								<option value="">Southeast Park</option>
+								<option value="">Third Street Park</option>
+								<option value="">Twin Lakes Sports Park</option>
+								<option value="">Upper Cascades Park</option>
+								<option value="">Wappehani Mountain Bike Park</option>
+								<option value="">Willie Streeter Community Gardens</option>
+								<option value="">Winslow Sports Complex</option>
+								<option value="">Winslow Woods Park</option>
+								</select>
+								</fieldset>
+							</form>
+						</div>
+					</div>
+						<div class="smright">
+							<h4>Health Insurance Info</h4>
+							<p><img src="http://rogue.bloomington.in.gov/drupal/sites/default/files/healthcarerectangle.png" /></p>
+						</div>
+					</div>
+				</li>
+			<li class="main-nav-item">
+				<a href="/content/students" class="main-nav-tab">Students</a>
+			</li>
+			<li class="main-nav-item">
+				<a href="/content/businesses" class="main-nav-tab">Businesses</a>
+			</li>
+			<li class="main-nav-item">
+				<a href="/content/residents" class="main-nav-tab">Residents</a>
+			</li>
+			<li class="main-nav-item">
+				<a href="/content/visitors" class="main-nav-tab">Visitors</a>
+			</li>
+			<li class="sm-links">
+				<a href="https://twitter.com/citybloomington">
+					<i class="icon-twitter"></i>
+					<span class="hidden-label">Twitter</span>
+				</a>
+			</li>
+			<li class="sm-links">
+				<a href="http://youtube.com/user/cityofbloomington">
+					<i class="icon-youtube"></i>
+					<span class="hidden-label">Youtube</span>
+				</a>
+			</li>
+			<li class="sm-links">
+				<a href="http://facebook.com/cityofbloomington">
+					<i class="icon-facebook"></i>
+					<span class="hidden-label">Facebook</span>
+				</a>
+			<li>
+		</ul>
