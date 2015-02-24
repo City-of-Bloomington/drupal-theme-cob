4aa505c0be14c39640165a4f59f259fbdef6ac3d
Dan Hiester <hiesterd@bloomington.in.gov>
2015-02-24 14:15:48 -0500

    Initial work on porting changes to html.tpl.php and page.tpl.php

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -49,16 +49,21 @@
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
-	<meta name="viewport" id="viewport" content="width=device-width, user-scalable=yes,initial-scale=1.0,maximum-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" id="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0,maximum-scale=1.0" />
   <?php print $styles; ?>
-  <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
+  <!--[if lt IE 9]>
+	  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
+  <?php print $scripts; ?>
 </body>
 </html>
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -73,84 +73,77 @@
  */
 ?>
 
-  <div id="page-wrapper"><div id="page">
-
-    <div id="header"><div class="section clearfix">
-
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
-
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name"><strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong></div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </h1>
+    <header class="btown-siteHeader">
+        <div class="btown-siteHeader-container">
+<?php /*
+            <?php if ($main_menu || $secondary_menu): ?>
+              <div id="navigation"><div class="section">
+                <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+                <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+              </div></div> <!-- /.section, /#navigation -->
             <?php endif; ?>
-          <?php endif; ?>
+*/ ?>
+            <?php if(!empty($page['header_site'])) {
+              echo render($page['header_site']);
+            } ?>
 
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /#name-and-slogan -->
-      <?php endif; ?>
+            <?php if ($logo): ?>
+                <a href="<?php print $front_page; ?>" class="btown-siteHeader-logo" title="<?php print t('Home'); ?>" rel="home" id="logo">
+                    <?php $alt_attribute = $site_name ? $site_name : t('Home'); ?>
+                    <img src="<?php print $logo; ?>" alt="<?php echo $alt_attribute; ?>" />
+                </a>
+            <?php endif; ?>
+        </div>
+    </header>
 
-      <?php print render($page['header']); ?>
+    <header class="btown-pageHeader">
+        <div class="btown-pageHeader-container">
+            <?php render($title_prefix) ?>
+            <h1><span><?php echo $title; ?></span></h1>
+            <?php print render($page['header_page']); ?>
+            <?php print render($title_suffix); ?>
 
-    </div></div> <!-- /.section, /#header -->
+        </div>
+        <div class="btown-pageHeader-navigation">
+            <nav class="btown-pageHeader-navigation-container">
+                <a href="#" class="btown-ext-current">About</a>
+                <a href="#">Other Page</a>
+            </nav>
+        </div>
+    </header>
+    <?php echo $messages; ?>
+    <?php
+       if($tabs || $action_links) {
+          echo "
+            <div class=\"btown-siteAdminBar\">
+                <div class=\"btown-SiteAdminBar-container\">
+          ";
+          if ($tabs){ echo "<div class=\"tabs\">".render($tabs)."</div> ";}
+          if ($action_links){ echo "<ul class=\"action-links\">".render($action_links)."</ul>";}
+          echo "
+                </div>
+            </div>
+          ";
+       }
+    ?>
 
-    <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-      </div></div> <!-- /.section, /#navigation -->
-    <?php endif; ?>
 
+<?php /*
     <?php if ($breadcrumb): ?>
       <div id="breadcrumb"><?php print $breadcrumb; ?></div>
     <?php endif; ?>
+*/ ?>
 
-    <?php print $messages; ?>
 
-    <div id="main-wrapper"><div id="main" class="clearfix">
-
-      <div id="content" class="column"><div class="section">
-        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
+
+
+
+
         <?php print $feed_icons; ?>
-      </div></div> <!-- /.section, /#content -->
 
-      <?php if ($page['sidebar_first']): ?>
-        <div id="sidebar-first" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_first']); ?>
-        </div></div> <!-- /.section, /#sidebar-first -->
-      <?php endif; ?>
+ 
 
-      <?php if ($page['sidebar_second']): ?>
-        <div id="sidebar-second" class="column sidebar"><div class="section">
-          <?php print render($page['sidebar_second']); ?>
-        </div></div> <!-- /.section, /#sidebar-second -->
-      <?php endif; ?>
-
-    </div></div> <!-- /#main, /#main-wrapper -->
-
-    <div id="footer"><div class="section">
-      <?php print render($page['footer']); ?>
-    </div></div> <!-- /.section, /#footer -->
-
-  </div></div> <!-- /#page, /#page-wrapper -->
+    <footer class="btown-footer"><div class="section">
+        <?php print render($page['footer']); ?>
+    </footer>
