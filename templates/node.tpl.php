a9159301231efee6654775086c65d489fe86927c
Dan Hiester <hiesterd@bloomington.in.gov>
2015-02-24 15:31:23 -0500

    First work on node.tpl.php, and adding stylesheets to repo

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -80,33 +80,39 @@
  * @ingroup themeable
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <?php print $user_picture; ?>
-
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
-
-  <?php print render($content['links']); ?>
-
-  <?php print render($content['comments']); ?>
-
+<div class="btown-pageSummary">
+	<div class="btown-pageSummary-container">
+		<article>
+			<?php echo render($content['summary']) ?>
+			<div class="btown-pageSummary-details">
+				<div class="btown-ext-details">
+					
+				</div>
+				<div class="btown-ext-details">
+					
+				</div>
+			</div>
+		</article>
+	</div>
 </div>
+<main id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> btown-main-container" role="main"<?php print $attributes; ?>>
+
+	<?php print $user_picture; ?>
+
+	<?php if ($display_submitted): ?>
+		<div class="submitted">
+			<?php print $submitted; ?>
+		</div>
+	<?php endif; ?>
+
+	<div class="content"<?php print $content_attributes; ?>>
+		<?php
+			// We hide the comments and links now so that we can render them later.
+			hide($content['summary']);
+			hide($content['telephone']);
+			hide($content['twitter']);
+			hide($content['facebook']);
+			echo render($content);
+		?>
+	</div>
+</main>
