5e5f255efba922d5b3cdaaeded67dd1fc9d1b51a
Dan Hiester <hiesterd@bloomington.in.gov>
2015-03-04 17:35:25 -0500

    Preliminary round of styling on document indexes for boards.

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -156,7 +156,7 @@
 		<?php
 			// We hide the comments and links now so that we can render them later.
 			hide($content['comments']);
-			//hide($content['links']);
+			hide($content['links']);
 			echo render($content);
 		?>
 	</article>
