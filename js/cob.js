44f936590ac747a98ad4cce16156749628e5c451
inghamn <inghamn@bloomington.in.gov>
2015-04-08 15:30:03 -0400

    Added javascript for Menu in banner Updates #244

--- js/cob.js
+++ js/cob.js
@@ -1,1 +1,21 @@
 "use strict";
+jQuery(function ($) {
+    var launcherClick = function(e)  {
+            var openMenus   = $('.menuLinks.open'),
+                menu        = $(e.target).siblings('.menuLinks');
+            openMenus.removeClass('open');
+            openMenus.addClass('closed');
+
+            menu.removeClass('closed');
+            menu.addClass('open');
+            e.stopPropagation();
+        },
+        documentClick = function(e) {
+            var openMenus   = $('.menuLinks.open');
+
+            openMenus.removeClass('open');
+            openMenus.addClass('closed');
+        };
+    $('.menuLauncher').on('click', launcherClick);
+    $(document).on('click', documentClick);
+});
