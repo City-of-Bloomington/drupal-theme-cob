c5f6186fa0b62b5daa4936e5c3352356682c9f9d
inghamn <inghamn@bloomington.in.gov>
2015-05-08 12:10:09 -0400

    Added external links to the taxonomy nodes display Fixes #287

--- includes/term_nodes.php
+++ includes/term_nodes.php
@@ -5,14 +5,38 @@
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param array $data['term']
  */
+$external_term_links = [
+    5 => [ // Getting Around Bloomington
+        [
+            'url'     => 'http://rogue.bloomington.in.gov/myCity',
+            'title'   => 'MyBloomington',
+            'summary' => 'Provides information about City services available to addresses in the vicinity of Bloomington, IN.'
+        ],
+    ]
+];
 ?>
 <div    class="cob-portalContent">
     <h2 class="cob-portalContent-heading"><?= $data['term']->name ?></h2>
     <?php
+        $items = [];
         $nodes = cob_taxonomy_nodes($data['term']->tid);
-        foreach($nodes as $node) {
-            $n = node_view($node, 'teaser');
-            echo render($n);
+        if ($nodes) {
+            foreach($nodes as $node) {
+                $n = node_view($node, 'teaser');
+                $items[$node->title] = render($n);
+            }
         }
+        if (!empty($external_term_links[$data['term']->tid])) {
+            foreach ($external_term_links[$data['term']->tid] as $link) {
+                $items[$link['title']] = "
+                <article class=\"cob-main-content\">
+                    <h2><a href=\"$link[url]\">$link[title]</a></h2>
+                    <div><p>$link[summary]</p></div>
+                </article>
+                ";
+            }
+        }
+        ksort($items);
+        foreach ($items as $html) { echo $html; }
     ?>
 </div>
