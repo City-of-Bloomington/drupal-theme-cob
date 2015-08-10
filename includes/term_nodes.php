8da2ccb68e2aefad669dfab1e7512407a33a0bf5
inghamn <inghamn@bloomington.in.gov>
2015-08-10 14:57:01 -0400

    We can now add RecTrac acitivity types to taxonomy term listings.
    Fixes #92

--- includes/term_nodes.php
+++ includes/term_nodes.php
@@ -5,6 +5,14 @@
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  * @param array $data['term']
  */
+
+/**
+ * If desired, you can add additional content items to be displayed to this
+ * $external_term_links array.  The index of the array should be the TID for the
+ * term you want to add content to.
+ *
+ * Items in this array will get merged and sorted into the list that's displayed.
+ */
 $external_term_links = [
     5 => [ // Getting Around Bloomington
         [
@@ -18,16 +26,33 @@ $external_term_links = [
 <div    class="cob-portalContent">
     <h2 class="cob-portalContent-heading"><?= $data['term']->name ?></h2>
     <?php
+        $tid = $data['term']->tid;
+
+        // Buffer the nodes so we can merge in additional content later
         $items = [];
-        $nodes = cob_taxonomy_nodes($data['term']->tid);
+        $nodes = cob_taxonomy_nodes($tid);
         if ($nodes) {
             foreach($nodes as $node) {
                 $n = node_view($node, 'teaser');
                 $items[$node->title] = render($n);
             }
         }
-        if (!empty($external_term_links[$data['term']->tid])) {
-            foreach ($external_term_links[$data['term']->tid] as $link) {
+
+        // Lookup RecTrac types and add them, as content nodes, to the current term listing.
+        if (!empty( $data['term']->field_rectrac_category)) {
+            $types = cob_rectrac_types($data['term']->field_rectrac_category['und'][0]['value']);
+            foreach ($types as $t) {
+                $external_term_links[$tid][] = [
+                    'url'     => 'https://bloomington.in.gov/webtrac/cgi-bin/wspd_cgi.sh/wbsearch.html?xxtype='.$t->Type,
+                    'title'   => $t->Descript,
+                    'summary' => ''
+                ];
+            }
+        }
+
+        // Merge in any content from $external_term_links
+        if (!empty(  $external_term_links[$tid])) {
+            foreach ($external_term_links[$tid] as $link) {
                 $items[$link['title']] = "
                 <article class=\"cob-main-content\">
                     <h2><a href=\"$link[url]\">$link[title]</a></h2>
