703b72442b356a44b3229feadab20bab7646abd9
Dan Hiester <hiesterd@bloomington.in.gov>
2015-11-16 17:57:56 -0500

    Convert board members layout to a horizontal layout.

--- includes/committeeMembers.php
+++ includes/committeeMembers.php
@@ -6,22 +6,24 @@
  * @param array $data['committee'] The committee object from the webservice call
  */
 ?>
-<h2>Members</h2>
-<dl class="cob-boardsCommissions-members">
-<?php
-    foreach ($data['committee']->seats as $seat) {
-        foreach ($seat->currentMembers as $member) {
-            $memberName = '';
-            $names = explode(' ', $member->name);
-            foreach($names as $n){
-                $memberName .= "<span>$n</span> ";
+<section class="cob-boardsCommissions-members">
+    <h1>Members</h1>
+    <?php
+        foreach ($data['committee']->seats as $seat) {
+            foreach ($seat->currentMembers as $member) {
+                $memberName = '';
+                $names = explode(' ', $member->name);
+                foreach($names as $n){
+                    $memberName .= "<span>$n</span> ";
+                }
+                echo "
+                <dl class=\"cob-boardsCommissions-member\">
+                    <dt>$memberName</dt>
+                    <dd>Appointed by: {$seat->appointedBy}</dd>
+                    <dd>Term expires: {$member->termEnd}</dd>
+                </dl>
+                ";
             }
-            echo "
-            <dt>$memberName</dt>
-            <dd>Appointed by: {$seat->appointedBy}</dd>
-            <dd>Term expires: {$member->termEnd}</dd>
-            ";
         }
-    }
-?>
-</dl>
+    ?>
+</section>
