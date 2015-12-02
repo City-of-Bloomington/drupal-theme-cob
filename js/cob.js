4ef3cf8c53b324f653ce3dbf1ba9936d04152999
Dan Hiester <hiesterd@bloomington.in.gov>
2015-12-02 16:06:13 -0500

    Merge public-board-vacancy branch to master.

--- js/cob.js
+++ js/cob.js
@@ -1,21 +1,38 @@
-"use strict";
-jQuery(function ($) {
-    var launcherClick = function(e)  {
-            var openMenus   = $('.menuLinks.open'),
-                menu        = $(e.target).siblings('.menuLinks');
-            openMenus.removeClass('open');
-            setTimeout(function() { openMenus.addClass('closed'); }, 300);
+(function () {
+    "use strict";
+    var closeMenus = function () {
+        var openLaunchers = document.querySelectorAll('.fn1-dropdown-launcher[aria-expanded="true"]'),
+            len = openLaunchers.length,
+            i   = 0;
+        for (i=0; i<len; i++) {
+            openLaunchers[i].setAttribute("aria-expanded", "");
+            openLaunchers[i].parentElement.focus();
+            openLaunchers[i].setAttribute("aria-expanded", "false");
+            (function (i) {
+                setTimeout(function() { openLaunchers[i].parentElement.querySelector('.fn1-dropdown-links').style.display = 'none'; }, 300);
+            })(i);
+        }
+        document.removeEventListener('click', closeMenus);
+    },
+    launcherClick = function(e) {
+        var launcher = e.target,
+            menu     = launcher.parentElement.querySelector('.fn1-dropdown-links');
+        launcher.blur();
+        closeMenus();
+        menu.style.display = 'block';
+        setTimeout(function() {
+            launcher.setAttribute("aria-expanded", "true");
+        }, 50);
+        document.addEventListener('click', closeMenus);
+        e.stopPropagation();
+        e.preventDefault();
+        menu.focus();
+    },
+    launchers = document.querySelectorAll('.fn1-dropdown-launcher'),
+    len   = launchers.length,
+    i = 0;
 
-            menu.removeClass('closed');
-            menu.   addClass('open');
-            e.stopPropagation();
-        },
-        documentClick = function(e) {
-            var openMenus   = $('.menuLinks.open');
-
-            openMenus.removeClass('open');
-            setTimeout(function() { openMenus.addClass('closed'); }, 300);
-        };
-    $('.menuLauncher').click(launcherClick);
-    $(document       ).click(documentClick);
-});
+    for (i=0; i<len; i++) {
+        launchers[i].addEventListener('click', launcherClick);
+    }
+})();
