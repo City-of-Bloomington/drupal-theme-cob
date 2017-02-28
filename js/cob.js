3d5ececfb7e4828e8e668af2e2e8c27eff286206
inghamn <inghamn@bloomington.in.gov>
2017-02-28 16:17:47 -0500

    Initial hamburger navigation implemented

--- js/cob.js
+++ js/cob.js
@@ -1,36 +1,36 @@
 (function () {
     "use strict";
     var closeMenus = function () {
-        var openLaunchers = document.querySelectorAll('.fn1-dropdown-launcher[aria-expanded="true"]'),
-            len = openLaunchers.length,
-            i   = 0;
-        for (i=0; i<len; i++) {
-            openLaunchers[i].setAttribute("aria-expanded", "");
-            openLaunchers[i].parentElement.focus();
-            openLaunchers[i].setAttribute("aria-expanded", "false");
-            (function (i) {
-                setTimeout(function() { openLaunchers[i].parentElement.querySelector('.fn1-dropdown-links').style.display = 'none'; }, 300);
-            })(i);
-        }
-        document.removeEventListener('click', closeMenus);
-    },
-    launcherClick = function(e) {
-        var launcher = e.target,
-            menu     = launcher.parentElement.querySelector('.fn1-dropdown-links');
-        launcher.blur();
-        closeMenus();
-        menu.style.display = 'block';
-        setTimeout(function() {
-            launcher.setAttribute("aria-expanded", "true");
-        }, 50);
-        document.addEventListener('click', closeMenus);
-        e.stopPropagation();
-        e.preventDefault();
-        menu.focus();
-    },
-    launchers = document.querySelectorAll('.fn1-dropdown-launcher'),
-    len   = launchers.length,
-    i = 0;
+            var openLaunchers = document.querySelectorAll('.dropdown [aria-expanded="true"]'),
+                openMenus     = document.querySelectorAll('.dropdown [aria-expanded="true"] + .links'),
+                len           = openLaunchers.length;
+            for (i=0; i<len; i++) {
+                openLaunchers[i].setAttribute("aria-expanded", "");
+                openLaunchers[i].parentElement.focus();
+                openLaunchers[i].setAttribute("aria-expanded", "false");
+                hideMenu(openMenus[i]);
+            }
+            document.removeEventListener('click', closeMenus);
+        },
+        hideMenu = function(m) {
+            m.setAttribute('hidden', 'hidden');
+        },
+        launcherClick = function(e) {
+            var launcher  = e.target,
+                container = launcher.parentElement,
+                menu      = launcher.parentElement.querySelector('.dropdown .links');
+            launcher.blur();
+            closeMenus();
+            menu.removeAttribute("hidden");
+            launcher.setAttribute('aria-expanded', 'true');
+            document.addEventListener('click', closeMenus);
+            e.stopPropagation();
+            e.preventDefault();
+            menu.focus();
+        },
+        launchers = document.querySelectorAll('.dropdown .launcher'),
+        len       = launchers.length,
+        i         = 0;
 
     for (i=0; i<len; i++) {
         launchers[i].addEventListener('click', launcherClick);
