67a0bc75ca4cede7820167cf60522094e58211b8
adabutch <butcherad@bloomington.in.gov>
2018-08-03 13:38:58 -0400

    social sharing via custom
    ref issue #146

--- js/cob.js
+++ js/cob.js
@@ -39,24 +39,39 @@
 	};
 
 	function truncateViaEllipses(text, max) {
-        return text.substr(0, max-1) + (text.length > max ? '...' : '');
-    };
+    return text.substr(0, max-1) + (text.length > max ? '...' : '');
+  };
 
-    /* Truncate News Titles
- 	 * locations at: / & /news
- 	 */
-    var newsTitleTextElm  = document.querySelectorAll('.news header h3 span'), i;
-    for (i = 0; i < newsTitleTextElm.length; ++i) {
-        var newTitleTexts = newsTitleTextElm[i].innerHTML;
-        newsTitleTextElm[i].innerHTML = truncateViaEllipses(newTitleTexts, 50);
-    }
+  /* Truncate News Titles
+	 * locations at: / & /news
+	 */
+  var newsTitleTextElm  = document.querySelectorAll('.news header h3 span'), i;
+  for (i = 0; i < newsTitleTextElm.length; ++i) {
+    var newTitleTexts = newsTitleTextElm[i].innerHTML;
+    newsTitleTextElm[i].innerHTML = truncateViaEllipses(newTitleTexts, 50);
+  };
 
-    /* Truncate News Summaries
- 	 * locations at: / & /news
- 	 */
-    var newsSummaryTextElm  = document.querySelectorAll('.news .summary'), i;
-    for (i = 0; i < newsSummaryTextElm.length; ++i) {
-        var newsSummaryTexts = newsSummaryTextElm[i].innerHTML;
-        newsSummaryTextElm[i].innerHTML = truncateViaEllipses(newsSummaryTexts, 120);
-    }
+  /* Truncate News Summaries
+	 * locations at: / & /news
+	 */
+  var newsSummaryTextElm  = document.querySelectorAll('.news .summary'), i;
+  for (i = 0; i < newsSummaryTextElm.length; ++i) {
+    var newsSummaryTexts = newsSummaryTextElm[i].innerHTML;
+    newsSummaryTextElm[i].innerHTML = truncateViaEllipses(newsSummaryTexts, 120);
+  };
+
+  function socialSharer(e){
+    var urlBase     = this.getAttribute("data-href");
+    var urlParams   = this.getAttribute("data-params");
+    window.open(urlBase + urlParams, "popup", "width=600,height=450");
+    console.log(urlBase + urlParams);
+  }
+
+  // Twitter Setter & Listener
+  var socialTwitterElement = document.getElementsByClassName('item twitter');
+  socialTwitterElement[0].addEventListener("click", socialSharer);
+
+  // Facebook Setter & Listener
+  var socialFacebookElement = document.getElementsByClassName('item facebook');
+  socialFacebookElement[0].addEventListener("click", socialSharer);
 })();
